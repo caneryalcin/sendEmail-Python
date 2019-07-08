fe74935e858faf80c5cbbd912240079b469050d0
caneryalcin <caneryalcin29@hotmail.com>
2019-07-08 14:48:32 +0300

    Delete sendEmail.py

--- emailsend-Python/send-email/sendEmail.py
+++
@@ -1,9 +0,0 @@
-import smtplib #SMTP is required to connect to the mail server and to send an e-mail.
-
-server = smtplib.SMTP('smtp.gmail.com', 587) #Connect a mail server and send it(587 is a port).
-server.starttls()
-
-server.login("username","password")
-server.sendmail("sender@gmail.com","receiver@gmail.com","Your message")
-
-server.quit()
